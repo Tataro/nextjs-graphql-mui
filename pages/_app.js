8da9628cb80893795260b38208878540adbf1a71
Romeo <romeo.kkg@gmail.com>
2021-02-04 12:17:41 +0700

    add mui

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,37 @@
 import { ApolloProvider } from "@apollo/client"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { ThemeProvider } from "@material-ui/core/styles"
+import Head from "next/head"
+import { useEffect } from "react"
 import { useApollo } from "../apollo/client"
+import theme from "../components/theme"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side")
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
   return (
-    <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
-    </ApolloProvider>
+    <>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <ApolloProvider client={apolloClient}>
+          <Component {...pageProps} />
+        </ApolloProvider>
+      </ThemeProvider>
+    </>
   )
 }
 
