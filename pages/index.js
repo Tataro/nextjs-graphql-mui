cbae7079b2cada1b98781073fb1467d63bade63d
Romeo <romeo.kkg@gmail.com>
2021-02-04 12:24:07 +0700

    test graphql

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,27 @@ import Box from "@material-ui/core/Box"
 import ProTip from "../components/ProTip"
 import Link from "../components/Link"
 import Copyright from "../components/Copyright"
+import { gql, useQuery } from "@apollo/client"
+
+const ViewerQuery = gql`
+  query ViewerQuery {
+    viewer {
+      id
+      name
+    }
+  }
+`
 
 export default function Index() {
+  const { data, loading, error } = useQuery(ViewerQuery)
+  const viewer = data?.viewer
+
+  console.log("data", data)
   return (
     <Container maxWidth="sm">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
+          Next.js example {viewer?.name}
         </Typography>
         <Link href="/about" color="secondary">
           Go to the about page
